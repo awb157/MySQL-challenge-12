48d35e21d7ede75ca9d9ce04f36c19b0c543e7e8
austin bennett <awb157@gmail.com>
2022-12-12 11:33:13 -0700

    seed and schema entered

--- schema.sql
+++ schema.sql
@@ -3,13 +3,21 @@ CREATE DATABASE emploee_db;
 USE emploee_db;
 
 CREATE TABLE departments(
-    id INT PRIMARY KEY AUTO_INCREMENT
+    id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles(
-    id INT PRIMARY KEY AUTO_INCREMENT
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    dapartment_id INT
 );
 
 CREATE TABLE emploees(
-    id INT PRIMARY KEY AUTO_INCREMENT
-)
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT
+);
